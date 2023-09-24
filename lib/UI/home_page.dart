2390a63467f03b044cd3b029b8e3f99b7af98bb9
Balghari <najmbalghari13@gmail.com>
2023-09-24 13:10:56 +0500

    add firebase realtime database post method to send data to database

--- lib/UI/home_page.dart
+++ lib/UI/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_pract/UI/Widget/rounded_btn.dart';
 import 'package:firebase_pract/UI/login_screen.dart';
 import 'package:firebase_pract/UI/utilis/toast_msg.dart';
@@ -15,6 +16,8 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
 FirebaseAuth _signOut = FirebaseAuth.instance;
 bool loading = false;
+final databaseRef = FirebaseDatabase.instance.ref('user');
+final textController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,11 +34,47 @@ ToastMsg().showToastMsg(error.toString());
           }, icon: Icon(Icons.logout_outlined))
         ],
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
+      body: Padding(
+        padding: const EdgeInsets.all(18.0),
+        child: Column(
+          //mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(height: 15,),
 
-        ],
+TextFormField(
+  controller: textController,
+  maxLines: 4,
+  decoration: InputDecoration(
+    hintText: 'Enter your data',
+    border: OutlineInputBorder(),
+  ),
+),
+            SizedBox(height: 15,),
+            RoundedBtn(btnName: 'Add',
+                isLoading: loading,
+                ontap: (){
+              setState(() {
+                loading= true;
+              });
+            databaseRef.child(DateTime.now().millisecondsSinceEpoch.toString()).set({
+              'name': textController.text.toString(),
+                'id': DateTime.now().millisecondsSinceEpoch,
+            }
+            ).then((value){
+              ToastMsg().showToastMsg('Added');
+              setState(() {
+                loading = false;
+              });
+            }).onError((error, stackTrace){
+              ToastMsg().showToastMsg(error.toString());
+              setState(() {
+                loading= false;
+              });
+            });
+
+            })
+          ],
+        ),
       ),
     );
   }
